cda07131799113f40b65573c8e521331c8d09e2d
Md.Rakibul Hasan <96834937+rakibuldipo@users.noreply.github.com>
2022-02-09 11:04:55 +0600

    first commit

---
+++ new1.js
@@ -0,0 +1,15 @@
+
+
+// ---------stype-1----------
+function makeRed() {
+    document.body.style.backgroundColor = 'red';
+}
+
+const bulebutton = document.getElementById('make-blue-button');
+bulebutton.onclick = makeBlue;
+function makeBlue() {
+    document.body.style.backgroundColor = 'blue';
+}
+
+
+const greenButton = document.getElementById('make-green-button');

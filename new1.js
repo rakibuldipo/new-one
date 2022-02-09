571a28c62d4693ffd911320432b4c8cd03c649cb
Md.Rakibul Hasan <96834937+rakibuldipo@users.noreply.github.com>
2022-02-09 11:27:29 +0600

    added header

--- new1.js
+++ new1.js
@@ -5,11 +5,17 @@ function makeRed() {
     document.body.style.backgroundColor = 'red';
 }
 
+// ---------stype-2----------
+
 const bulebutton = document.getElementById('make-blue-button');
 bulebutton.onclick = makeBlue;
 function makeBlue() {
     document.body.style.backgroundColor = 'blue';
 }
 
+// ---------stype-3----------
 
 const greenButton = document.getElementById('make-green-button');
+greenButton.onclick = function makeGreen() {
+    document.body.style.backgroundColor = 'green';
+}
